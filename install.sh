852fd16992d011d7f5e9e0b70ef03fcd21821404
Vishal Kaslwial <vishal.kasliwal@gmail.com>
2017-07-13 14:45:52 -0700

    Added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd ../..
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+cd ../../../..
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/lld/trunk lld
+cd ../..
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/polly/trunk polly
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp
+cd ../..
+mkdir build
+cd build
+cmake -G "Unix Makefiles" ../llvm
+make
